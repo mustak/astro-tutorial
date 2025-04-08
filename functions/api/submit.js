5436857b36484d498e1d8eb8bb35c4bc59d2c14b
mustak <mhyaqub@yahoo.com>
2025-04-08 18:01:28 +0100

    cloudflare redirect

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -5,11 +5,13 @@ export async function onRequestPost(context) {
     try {
         let input = await context.request.formData();
         let pretty = JSON.stringify(input, null, 2);
-        return new Response(null, {
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-        });
+        // return new Response(null, {
+        //     headers: {
+        //         'Content-Type': 'application/json;charset=utf-8',
+        //     },
+        // });
+        const response = new Response(null);
+        return response.redirected('/about', 302);
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
