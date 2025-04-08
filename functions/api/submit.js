e0e00f709b6294a44b99185149e2be1c3d9a48ff
mustak <mhyaqub@yahoo.com>
2025-04-08 18:10:37 +0100

    add header

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -5,9 +5,14 @@ export async function onRequestPost(context) {
     try {
         let input = await context.request.formData();
         let pretty = JSON.stringify(input, null, 2);
-        return new Response(pretty, {
+        // return new Response(pretty, {
+        //     headers: {
+        //         'Content-Type': 'application/json;charset=utf-8',
+        //     },
+        // });
+        return new Response(null, {
             headers: {
-                'Content-Type': 'application/json;charset=utf-8',
+                location: '/about',
             },
         });
         // const response = new Response(null);
