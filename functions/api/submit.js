0d1df6176b09b96ea632a85a65b1145ae847176a
mustak <mhyaqub@yahoo.com>
2025-04-08 17:42:52 +0100

    print formdata

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -4,13 +4,12 @@
 export async function onRequestPost(context) {
     try {
         let input = await context.request.formData();
-        let pretty = JSON.stringify([...input], null, 2);
-        // return new Response(pretty, {
-        //     headers: {
-        //         'Content-Type': 'application/json;charset=utf-8',
-        //     },
-        // });
-        return new Response().redirected('/about');
+        let pretty = JSON.stringify(input, null, 2);
+        return new Response(null, {
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+        });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
