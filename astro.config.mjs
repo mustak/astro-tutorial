3b903f737bb41b39c73c2b40f4dc62294bb59b42
mustak <mhyaqub@yahoo.com>
2025-04-15 14:01:33 +0100

    digital-edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-
-    integrations: [react()],
+  site: "https://digital-edge.uk",
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
